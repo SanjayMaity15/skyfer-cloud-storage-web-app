3317d9febbb658aa315f44aebda3054421b807e7
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-02-28 11:10:03 +0530

    nodemailer added

---
+++ server/config/sendMailNodeMailer.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Create a transporter using Ethereal test credentials.
+// For production, replace with your actual SMTP server details.
+const transporter = nodemailer.createTransport({
+	host: "smtp.ethereal.email",
+	port: 587,
+	secure: false, // Use true for port 465, false for port 587
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_APP_PASS,
+	},
+});
+
+// Send an email using async/await
+
+export const sendMailUsingNodeMailer = async (email, subject, otp = null) => {
+	const info = await transporter.sendMail({
+		from: `"Skyfer" <${process.env.EMAIL_USER}>`,
+		to: email,
+		subject: subject,
+		html: `<p>Your registration <b>OTP : ${otp}</b> for Skyfer</p>`,
+	});
+
+	console.log("Message sent:", info.messageId);
+};
