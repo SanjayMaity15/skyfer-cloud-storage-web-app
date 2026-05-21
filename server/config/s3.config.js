78504bec0cb3c2129c0d0e5b476148dd6ab3778e
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-21 17:52:33 +0530

    backend done

--- server/config/s3.config.js
+++ server/config/s3.config.js
@@ -1,4 +1,5 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -10,3 +11,27 @@ export const s3 = new S3Client({
 		secretAccessKey: process.env.AWS_SECRET_KEY,
 	},
 });
+
+
+
+export const getObject = async (key, download = false, filename = "file") => {
+	try {
+		const command = new GetObjectCommand({
+			Bucket: process.env.S3_BUCKET_NAME,
+			Key: key,
+
+			// THIS controls preview vs download
+			ResponseContentDisposition: download
+				? `attachment; filename="${encodeURIComponent(filename)}"`
+				: "inline",
+		});
+
+		const url = await getSignedUrl(s3, command, {
+			expiresIn: 3600,
+		});
+
+		return url;
+	} catch (error) {
+		console.log(error);
+	}
+};
