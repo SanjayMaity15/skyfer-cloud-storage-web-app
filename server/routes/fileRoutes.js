0013da247f2d21c8c1686070e01ac3772eca0b1c
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-21 17:51:32 +0530

    routes add

--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -1,14 +1,16 @@
 import express from "express"
 import { upload } from "../services/multer.js";
-import { deleteFile, fileUpload, getDeletedFile, permanantDelete, recoverDeletedFile } from "../controllers/fileController.js";
+import { deleteFile, fileUploadInitiate, getDeletedFile, permanantDelete, recoverDeletedFile, verifyFileUploadComplete } from "../controllers/fileController.js";
 import { isAuth } from "../middlewares/isAuth.js";
 import { sendFileToUser } from "../controllers/fileController.js";
 import { renameFile } from "../controllers/fileController.js";
 
 const router = express.Router()
 
-router.post("/upload", isAuth, upload.single("file"), fileUpload)
-router.post("/upload/:id", isAuth, upload.single("file"), fileUpload)
+router.post("/upload/initiate", isAuth, fileUploadInitiate)
+router.post("/upload/complete", isAuth, verifyFileUploadComplete)
+// router.post("/upload", isAuth, upload.single("file"), fileUpload)
+// router.post("/upload/:id", isAuth, upload.single("file"), fileUpload)
 router.get("/view/:id", isAuth, sendFileToUser)
 router.post("/rename/:id", isAuth, renameFile)
 router.delete("/delete/:id", isAuth, deleteFile)
